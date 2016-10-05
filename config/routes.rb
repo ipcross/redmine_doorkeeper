e1d3cf945f2a0721aec8e1649b87feb456659844
cross <cross@vgg.ru>
2016-10-05 12:02:16 +0300

    Init commit.

---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+# -*- encoding : utf-8 -*-
+# Plugin's routes
+# See: http://guides.rubyonrails.org/routing.html
---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_doorkeeper do
+  name 'Redmine Doorkeeper plugin'
+  author 'Alifanov Dmitry'
+  description 'Oauth2 Provider for Redmine'
+  version '0.0.1'
+  url 'http://www.is.vgg.ru'
+  author_url 'http://www.is.vgg.ru'
+end
